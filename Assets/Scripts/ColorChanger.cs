fbe836c47cf0ef7f88ac4ac86ad130f7c69f6271
Sergey Antonov <Antonov-s@mail.ru>
2023-07-25 20:12:58 +0400

    added new color

--- Assets/Scripts/ColorChanger.cs
+++ Assets/Scripts/ColorChanger.cs
@@ -5,16 +5,29 @@ using UnityEngine;
 public class ColorChanger : MonoBehaviour
 {
     [HideInInspector] public SpriteRenderer target;
-
     [SerializeField] private float _duration;
+    [SerializeField] private Color _targetColor;
+    private float _runningTime;
+    private Color _statrColor;
 
     private void Start()
     {
         target = GetComponent<SpriteRenderer>();
+        _statrColor = target.color;
     }
 
     void Update()
     {
-        
+        if (_runningTime <= _duration)
+        {
+            _runningTime += Time.deltaTime;
+            float notmalizeRunningTime = _runningTime / _duration;
+
+            // Color newColor = new Color(_targetColor.r * notmalizeRunningTime, _targetColor.g * notmalizeRunningTime, _targetColor.b * notmalizeRunningTime);
+            //  target.color = newColor;
+
+            target.color = Color.Lerp(_statrColor, _targetColor, notmalizeRunningTime);
+        }
+
     }
 }
